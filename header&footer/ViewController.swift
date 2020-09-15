8c12f3a85d297a343461650b40ca2442f4a406c4
Shelly <shelly980102@gmail.com>
2020-09-16 00:21:57 +0800

    Initial Commit

---
+++ header&footer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  header&footer
+//
+//  Created by 蕭聿莘 on 2020/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
