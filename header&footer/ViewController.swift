485cc347da893a9603bf7a86d9802bb8eb45b9e7
Shelly <shelly980102@gmail.com>
2020-09-20 11:21:11 +0800

    header&footer

--- header&footer/ViewController.swift
+++ header&footer/ViewController.swift
@@ -9,11 +9,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var aTableView: UITableView!
+    
+    let fruits = [
+        ["apple", "banana", "durian",  "plum"],
+        ["金", "木", "水", "火", "土"],
+        ["🦊"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        aTableView.dataSource = self
+        aTableView.delegate = self
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: aTableView.frame.width, height: 300 ))
+        let footerrView = UIView(frame: CGRect(x: 0, y: 0, width: aTableView.frame.width, height: 100 ))
+        
+        
+        headerView.backgroundColor = .purple
+        footerrView.backgroundColor = .blue
+        
+        aTableView.tableHeaderView = headerView
+        aTableView.tableFooterView = footerrView
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Header\(section)"
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        if section == 2 {
+            return 300
+        }else{
+            return 0
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "aCell", for: indexPath)
+        cell.textLabel?.text = fruits[indexPath.section][indexPath.row]
+        cell.backgroundColor = .green
+        return cell
+    }
+    
+    
+}
